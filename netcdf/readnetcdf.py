a53d39e0f4fea52c4c3cf7e653296976a8142826
Christian Mikovits <christian.mikovits@boku.ac.at>
2021-05-06 16:33:49 +0200

    working example

--- netcdf/readnetcdf.py
+++ netcdf/readnetcdf.py
@@ -1,30 +1,65 @@
-from netCDF4 import Dataset
+import xarray
 import numpy as np
 import datetime
 import suntimes
+import pyproj
+import geopandas as gpd
+import pandas as pd
+import math
+import cftime
+from shapely.geometry import Point, Polygon
+import matplotlib.pyplot as plt
+import pvlib
+
+def cccanccoords(nd, point, csrs = 'epsg:4326'):
+    abslat = np.abs(nd.lat-coords['geometry'].y[0])
+    abslon = np.abs(nd.lon-coords['geometry'].x[0])
+    c = np.maximum(abslon, abslat)
+
+    ([yloc], [xloc]) = np.where(c == np.min(c))
+    return(nd['x'][xloc].values, nd['y'][yloc].values)
+
+day = cftime.Datetime360Day(2050, 7, 1, 12, 0, 0, 0)
+print(day.strftime('%j'))
+
+coords = pd.DataFrame(
+    {'name': ['Vienna'],
+     'lat': [48.210033],
+     'lon': [16.363449] })
+
+geometry = [Point(xy) for xy in zip(coords.lon, coords.lat)]
+coords = coords.drop(['lon', 'lat'], axis=1)
+coords = gpd.GeoDataFrame(coords, crs="epsg:4326", geometry=geometry)
+
+print(coords)
 
 nc_file = '/home/cmikovits/Downloads/rsds_SDM_MOHC-HadGEM2-ES_rcp45_r1i1p1_CLMcom-CCLM4-8-17.nc'
-nd = Dataset(nc_file, mode='r')
+nd = xarray.open_dataset(nc_file)
 
-#print(nd)
-print(nd['time'])
-print(nd['rsds'])
+nx, ny = cccanccoords(nd, coords)
+res = nd.sel(x=nx, y=ny, time = day, method = 'nearest')
 
-day = datetime.datetime(2021,7,1)
+print(nd.title)
+
+print(res['rsds'].values)
+
 place = suntimes.SunTimes(9.3, 41.6, altitude=200)
 print(place.riseutc(day))
 
-jan = 30+360*50+0.5
-jul = jan + 6*30
+location = pvlib.location.Location(
+    coords['geometry'].y,
+    coords['geometry'].x,
+    'Europe/Vienna',
+    250,
+    'Vienna-Austria')
 
-print(nd['rsds'][jan, 200, 300])
-print(nd['rsds'][jul, 200, 300])
-
-#lons = nd.variables['lon'][1]
-#lats = nd.variables['lat'][1]
-#time = nd.variables['time'][1:100]
-#rsds = nd.variables['rsds'][tsl, 40, 5]
-
-
-#print(min(lons), max(lons), min(lats), max(lats))
-#print(time,rsds)
+dtseries = pd.date_range(start='2020-07-01', end='2020-07-3', periods=49)
+ghiseries = pd.DataFrame({'GHI':[0, 0, 0, 0, 10, 20, 30, 50, 70, 80, 90, 110, 140, 110, 90, 80, 70, 50, 30, 20, 10, 0, 0, 0, 0 ]})
+print(dtseries)
+solar_position = location.get_solarposition(dtseries[13])
+print(solar_position)
+disc = pvlib.irradiance.disc(
+    500,
+    solar_position['apparent_zenith'].values,
+    dtseries[13])
+print(disc)
