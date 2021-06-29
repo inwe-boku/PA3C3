ea401616f8f6e5a7fe9813d4ae687e12a0671cb8
Christian Mikovits <christian.mikovits@boku.ac.at>
2021-06-29 14:14:16 +0200

    typer

--- netcdf/readnetcdf.py
+++ netcdf/readnetcdf.py
@@ -1,3 +1,4 @@
+import typer
 import xarray
 import numpy as np
 import datetime
@@ -217,142 +218,164 @@ def calc_pvoutput(point, ts_rtw, tracking, capacity_kWp, tz='UTC'):
                  installed_capacity_kWp).fillna(0)
     return pv_output
 
+def writeGEO(data, path, dataname, types = {'geojson': 0, 'shape': 0, 'gpkg': 1}):
+    if 'geojson' in types:
+        data.to_file(filename = os.path.join(path, 'geojson', dataname+'.geojson'), driver="GeoJSON")
+    if 'shapes' in types:
+        data.to_file(filename = os.path.join(path, 'shape', dataname+'.shp'), driver = 'ESRI Shapefile')
+    if 'gpkg' in types:
+        data.to_file(filename = os.path.join(path, 'data.gpkg'), layer = dataname, driver = 'GPKG')
+    return(0)
 
+def attic():
+    mydate = cftime.Datetime360Day(2050, 6, 20, 12, 0, 0, 0)
+    print(mydate.strftime('%j'))
 
-mydate = cftime.Datetime360Day(2050, 6, 20, 12, 0, 0, 0)
-print(mydate.strftime('%j'))
+    coords = pd.DataFrame(
+        {'name': ['Vienna','BruckNeudorf'],
+         'lat': [48.21003, 48.02261],
+         'lon': [16.36344, 16.83951] }
+                         )
+    geometry = [Point(xy) for xy in zip(coords.lon, coords.lat)]
+    point = coords.drop(['lon', 'lat'], axis=1)
+    point = gpd.GeoDataFrame(coords, crs="epsg:4326", geometry=geometry)
 
-coords = pd.DataFrame(
-    {'name': ['Vienna','BruckNeudorf'],
-     'lat': [48.21003, 48.02261],
-     'lon': [16.36344, 16.83951] }
-                     )
-geometry = [Point(xy) for xy in zip(coords.lon, coords.lat)]
-point = coords.drop(['lon', 'lat'], axis=1)
-point = gpd.GeoDataFrame(coords, crs="epsg:4326", geometry=geometry)
+    print(point)
 
-print(point)
+    nc_file = '/home/cmikovits/Downloads/rsds_SDM_MOHC-HadGEM2-ES_rcp45_r1i1p1_CLMcom-CCLM4-8-17.nc'
+    nd = xarray.open_dataset(nc_file)
 
-nc_file = '/home/cmikovits/Downloads/rsds_SDM_MOHC-HadGEM2-ES_rcp45_r1i1p1_CLMcom-CCLM4-8-17.nc'
-nd = xarray.open_dataset(nc_file)
+    nx, ny = calc_ccca_xy(nd, point)
+    res = get_ccca_values(nd, nx, ny, mydate)
+    rsds_value = res['rsds'].values
 
-nx, ny = calc_ccca_xy(nd, point)
-res = get_ccca_values(nd, nx, ny, mydate)
-rsds_value = res['rsds'].values
+    w_s = sunset_azimuth(point, mydate)
+    print(w_s)
+    exit(0)
 
-w_s = sunset_azimuth(point, mydate)
-print(w_s)
-exit(0)
+    location = pvlib.location.Location(
+        coords['geometry'].y,
+        coords['geometry'].x,
+        'Europe/Vienna',
+        250, #müa
+        'Vienna-Austria')
 
-location = pvlib.location.Location(
-    coords['geometry'].y,
-    coords['geometry'].x,
-    'Europe/Vienna',
-    250, #müa
-    'Vienna-Austria')
-
-dates = pd.date_range(start='2020-07-01', end='2020-07-03')
-
-
-
-### sun location
-
-for date in dates:
-    settime = place.setutc(date)
-    solar_position = location.get_solarposition(settime)
-    zenith_sunset = solar_position['apparent_zenith'].values[0] #sunset azimuth
-    w_s = solar_position['azimuth'].values[0]
-    print('rsds', rsds_value)
-
-    datetimes = pd.date_range(start=date, end=date + + datetime.timedelta(hours=23), freq='H')
-    rad = {'settime': settime,
-           'w_s': w_s}
-    w_s_adp = w_s - 180
-    rad_w_s_adp = math.radians(w_s_adp)
-    cos_w_s = math.cos(rad_w_s_adp)
-    sin_w_s = math.sin(rad_w_s_adp)
-
-    ### for collares-pereira model
-    w_s_cp = w_s_adp - 60
-    sin_w_s_cp = math.sin(math.radians(w_s_cp))
-
-    a = 0.409+(0.5016*sin_w_s_cp)
-    b = 0.6609-(0.4767*sin_w_s_cp)
-
-    data = pd.DataFrame(index = datetimes, columns = {'w_h', 'z_h', 'dni_disc', 'dni_erbs', 'dhi_erbs', 'r_h', 'G_h', 'r_cp'})
-    for dt in datetimes:
-        solar_position = location.get_solarposition(dt)
-        w_h = solar_position['azimuth'].values[0]  ### azimuth of sun
-        data['w_h'].loc[dt] = w_h
-        z_h = solar_position['zenith'].values[0]   ### zenith of sun
-        data['z_h'].loc[dt] = z_h
-
-        w_h_adp = 180 - w_h
-        cos_w_h = math.cos(math.radians(w_h_adp))
-        r_h = (math.pi/24 * (cos_w_h - cos_w_s)) / ( sin_w_s - (rad_w_s_adp * cos_w_s)) # Liu Jordan formula
-        r_h = np.clip(r_h, a_min = 0, a_max=None)
-
-        r_cp = (a + b * cos_w_h) * math.pi/24 * (cos_w_h - cos_w_s) / (sin_w_s - (rad_w_s_adp * cos_w_s))
-
-        # formulas from: https://www.hindawi.com/journals/ijp/2015/968024/ #1
-        data['r_h'].loc[dt] = r_h
-
-        dni_disc = pvlib.irradiance.disc(
-            r_h,
-            z_h,
-            dt)['dni']
-        data['dni_disc'].loc[dt] = dni_disc
-        dni_erbs = pvlib.irradiance.erbs(r_h,z_h,dt)
-        data['dni_erbs'].loc[dt] = dni_erbs['dni']
-        data['dhi_erbs'].loc[dt] = dni_erbs['dhi']
-        data['G_h'].loc[dt] = r_h * rsds_value * 24
-        data['r_cp'].loc[dt] = r_cp
-    # r_h = np.clip(r_h, a_min = 0, a_max=None).tolist()
-
-    print(data)
+    dates = pd.date_range(start='2020-07-01', end='2020-07-03')
 
 
 
-exit(0)
-### horizon / terrain calculation
+    ### sun location
 
-options = gdal.WarpOptions(cutlineDSName="/home/cmikovits/myshape.shp",cropToCutline=True)
-outBand = gdal.Warp(srcDSOrSrcDSTab="/home/cmikovits/GEODATA/DHMAT/dhm_at_lamb_10m_2018.tif",
-                        destNameOrDestDS="/tmp/cut.tif",
-                        options=options)
-outBand = None
+    for date in dates:
+        settime = place.setutc(date)
+        solar_position = location.get_solarposition(settime)
+        zenith_sunset = solar_position['apparent_zenith'].values[0] #sunset azimuth
+        w_s = solar_position['azimuth'].values[0]
+        print('rsds', rsds_value)
+
+        datetimes = pd.date_range(start=date, end=date + + datetime.timedelta(hours=23), freq='H')
+        rad = {'settime': settime,
+               'w_s': w_s}
+        w_s_adp = w_s - 180
+        rad_w_s_adp = math.radians(w_s_adp)
+        cos_w_s = math.cos(rad_w_s_adp)
+        sin_w_s = math.sin(rad_w_s_adp)
+
+        ### for collares-pereira model
+        w_s_cp = w_s_adp - 60
+        sin_w_s_cp = math.sin(math.radians(w_s_cp))
+
+        a = 0.409+(0.5016*sin_w_s_cp)
+        b = 0.6609-(0.4767*sin_w_s_cp)
+
+        data = pd.DataFrame(index = datetimes, columns = {'w_h', 'z_h', 'dni_disc', 'dni_erbs', 'dhi_erbs', 'r_h', 'G_h', 'r_cp'})
+        for dt in datetimes:
+            solar_position = location.get_solarposition(dt)
+            w_h = solar_position['azimuth'].values[0]  ### azimuth of sun
+            data['w_h'].loc[dt] = w_h
+            z_h = solar_position['zenith'].values[0]   ### zenith of sun
+            data['z_h'].loc[dt] = z_h
+
+            w_h_adp = 180 - w_h
+            cos_w_h = math.cos(math.radians(w_h_adp))
+            r_h = (math.pi/24 * (cos_w_h - cos_w_s)) / ( sin_w_s - (rad_w_s_adp * cos_w_s)) # Liu Jordan formula
+            r_h = np.clip(r_h, a_min = 0, a_max=None)
+
+            r_cp = (a + b * cos_w_h) * math.pi/24 * (cos_w_h - cos_w_s) / (sin_w_s - (rad_w_s_adp * cos_w_s))
+
+            # formulas from: https://www.hindawi.com/journals/ijp/2015/968024/ #1
+            data['r_h'].loc[dt] = r_h
+
+            dni_disc = pvlib.irradiance.disc(
+                r_h,
+                z_h,
+                dt)['dni']
+            data['dni_disc'].loc[dt] = dni_disc
+            dni_erbs = pvlib.irradiance.erbs(r_h,z_h,dt)
+            data['dni_erbs'].loc[dt] = dni_erbs['dni']
+            data['dhi_erbs'].loc[dt] = dni_erbs['dhi']
+            data['G_h'].loc[dt] = r_h * rsds_value * 24
+            data['r_cp'].loc[dt] = r_cp
+        # r_h = np.clip(r_h, a_min = 0, a_max=None).tolist()
+
+        print(data)
 
 
-ds = gdal.Open("/tmp/cut.tif")
-#print(ds.info())
-#gt = ds.GetGeoTransform()
-dem = np.array(ds.GetRasterBand(1).ReadAsArray())
-dem = dem.astype(np.double)
 
-print(dem)
+    exit(0)
+    ### horizon / terrain calculation
 
-dem_spacing = 10
-
-hrz = horizon(0, dem, dem_spacing)
-#slope, aspect = gradient_d8(dem, dem_spacing, dem_spacing)
-#svf, tvf = viewf(dem, spacing=dem_spacing)
-
-print(hrz)
-
-plt.imshow(hrz)
-plt.show()
-
-### PV System Modelling
-
-modules = pvlib.pvsystem.retrieve_sam('SandiaMod')
-inverter = pvlib.pvsystem.retrieve_sam('cecinverter')
-inverter = inverter['ABB__MICRO_0_25_I_OUTD_US_208__208V_']
-temperature_m = pvlib.temperature.TEMPERATURE_MODEL_PARAMETERS['sapm']['open_rack_glass_glass']
+    options = gdal.WarpOptions(cutlineDSName="/home/cmikovits/myshape.shp",cropToCutline=True)
+    outBand = gdal.Warp(srcDSOrSrcDSTab="/home/cmikovits/GEODATA/DHMAT/dhm_at_lamb_10m_2018.tif",
+                            destNameOrDestDS="/tmp/cut.tif",
+                            options=options)
+    outBand = None
 
 
-system = pvlib.pvsystem.PVSystem(surface_tilt=20, surface_azimuth=180,
-                                 module_parameters = modules,
-                                 inverter_parameters = inverter,
-                                 temperature_model_parameters = temperature_m)
+    ds = gdal.Open("/tmp/cut.tif")
+    #print(ds.info())
+    #gt = ds.GetGeoTransform()
+    dem = np.array(ds.GetRasterBand(1).ReadAsArray())
+    dem = dem.astype(np.double)
 
-print(system)
+    print(dem)
+
+    dem_spacing = 10
+
+    hrz = horizon(0, dem, dem_spacing)
+    #slope, aspect = gradient_d8(dem, dem_spacing, dem_spacing)
+    #svf, tvf = viewf(dem, spacing=dem_spacing)
+
+    print(hrz)
+
+    plt.imshow(hrz)
+    plt.show()
+
+    ### PV System Modelling
+
+    modules = pvlib.pvsystem.retrieve_sam('SandiaMod')
+    inverter = pvlib.pvsystem.retrieve_sam('cecinverter')
+    inverter = inverter['ABB__MICRO_0_25_I_OUTD_US_208__208V_']
+    temperature_m = pvlib.temperature.TEMPERATURE_MODEL_PARAMETERS['sapm']['open_rack_glass_glass']
+
+
+    system = pvlib.pvsystem.PVSystem(surface_tilt=20, surface_azimuth=180,
+                                     module_parameters = modules,
+                                     inverter_parameters = inverter,
+                                     temperature_model_parameters = temperature_m)
+
+    print(system)
+
+def main(name: str):
+    typer.echo(f"Hello {name}")
+    parser = GooeyParser(description="My Cool GUI Program!")
+    parser.add_argument('LanduseFile', help="Raster File with LU values", widget='FileChooser')
+    parser.add_argument('RadiationFile', help="CCCA netcdf with radiation data", widget='FileChooser')
+    parser.add_argument('ConfigurationFile', help="Configuration file", widget='FileChooser')
+
+
+    config = getyml(os.path.join(os.path.dirname(os.path.realpath(__file__)), configfile))
+    jrccfg = getyml(os.path.join(os.path.dirname(os.path.realpath(__file__)), jrcconfig))
+
+    areafile = os.path.join(inpdir, areaname, 'area.shp')
+    area = gpd.read_file(areafile)
