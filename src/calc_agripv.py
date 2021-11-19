4fa84e8c2041ba4bd992b79afdf3a22c5d377c9f
Christian Mikovits <christian.mikovits@boku.ac.at>
2021-11-19 14:00:58 +0100

    RECHECK DAILY2HOURLY

--- src/calc_agripv.py
+++ src/calc_agripv.py
@@ -60,6 +60,8 @@ def calc_ccca_xy(nd, point):
     c = np.maximum(abslon, abslat)
 
     ([yloc], [xloc]) = np.where(c == np.min(c))
+    print(point)
+    print(nd['x'][xloc].values, nd['y'][yloc].values)
     return(nd['x'][xloc].values, nd['y'][yloc].values)
 
 
@@ -408,8 +410,9 @@ def cccapoints(nd, points, daterange, daterange365):
         # nxny.append(nxnykey)
         if not nxnykey in cccadict.keys():
             res = get_ccca_values(nd, nx, ny, daterange)
-            rsds_values = res['rsds'].values
-            values = values_day360_day365(rsds_values)
+            #rsds_values = res['rsds'].values
+            #values = values_day360_day365(rsds_values)
+            values = res.rsds.values
             cccadict[nxnykey] = {}
             cccadict[nxnykey]['rsds'] = values  # fill numpy ndarray
             cccadict[nxnykey]['date'] = daterange365
@@ -617,7 +620,7 @@ def main(t_path: Path = typer.Option(DEFAULTPATH, "--path", "-p"),
          t_configfile: Path = typer.Option(
              "cfg/testcfg.yml", "--config", "-c"),
          t_cccancfile: str = typer.Option(
-             "/data/projects/PA3C3/Input/rsds_SDM_NCC-NorESM1-M_rcp85_r1i1p1_DMI-HIRHAM5_all.nc", "--cccanc", "-nc"),
+             "/data/projects/PA3C3/Input/rsds_SDM_IPSL-IPSL-CM5A-MR_rcp85_r1i1p1_IPSL-INERIS-WRF331F.nc", "--cccanc", "-nc"),
          t_dhmfile: str = typer.Option(
              "/data/projects/PA3C3/Input/dhm_at_lamb_10m_2018.tif", "--dhm", "-dhm"),
          t_horfile: str = typer.Option(
@@ -773,6 +776,9 @@ def main(t_path: Path = typer.Option(DEFAULTPATH, "--path", "-p"),
         config['ccca']['startyears'], config['ccca']['timeframe'])
     dates365 = gendates365(
         config['ccca']['startyears'], config['ccca']['timeframe'])
+    ### for other files we do not need dates360
+    
+    dates360 = dates365
 
     for year, daterange in dates360.items():
         daterange365 = dates365[year]
@@ -787,7 +793,10 @@ def main(t_path: Path = typer.Option(DEFAULTPATH, "--path", "-p"),
                 geom.y, geom.x,
                 'UTC', altitude, nxny)
             # GHI daily to GHI hourly
+            ddf = pd.DataFrame(ddata)
+            ddf.to_csv('dailyraw.csv')
             hdata = ghid2ghih(ddata, daterange, location)
+            hdata.to_csv('hourlyraw.csv')
             hdata['location'] = geom
             # DNI+DHI hourly
             hdata = ghi2dni(hdata, config['pvmod']['hmodel'])
